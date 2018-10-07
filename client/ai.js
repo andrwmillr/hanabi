08413c8027461e891032cdcbe8029b0f51f8053d
Andrew Miller <admiller9@gmail.com>
2018-10-07 12:55:26 -0400

    gitignore fix

--- client/ai.js
+++
@@ -1,8 +0,0 @@
-// import {moves} from './functions'
-
-// export const runAI = (game, counter) => {
-//   const hands = game.hands
-//   const board = game.board
-//   console.log(game)
-//   return moves.play(game, 'Ava', hands.Ava[0])
-// }
