f6d9b38e6d4dd7f7c5834634042641e8a7a2f63c
Andrew Miller <admiller9@gmail.com>
2018-10-09 19:14:50 -0400

    edits to walkthrough

--- client/components/Walkthrough.js
+++ client/components/Walkthrough.js
@@ -6,16 +6,27 @@ const Walkthrough = props => {
       <div id="walkthrough-content" className="vert-flex">
         <div className="walkthrough-div">
           <p className="walkthrough-text">
-            To begin, <b>each player is dealt five cards</b>. <br /> You are{' '}
-            <b>not allowed to see you own hand</b>, but you can see other
-            players' hands.
+            Hanabi is an award-winning <b>cooperative card game</b>, designed by
+            Antoine Bauza and published in 2010.
+          </p>
+          <p className="walkthrough-text" style={{ margin: '0' }}>
+            <b>"Hanabi" is the Japanese word for firework</b>. The game's
+            premise is that players are trying to produce the most impressive
+            fireworks show they can.
+          </p>
+        </div>
+        <div className="walkthrough-div">
+          <p className="walkthrough-text">
+            To begin, <b>each player is dealt five cards</b>. <br /> You{' '}
+            <b>cannot to see you own hand</b>, but you can see other players'
+            hands.
           </p>
           <p className="walkthrough-text">
             During your turn, you have three options. <br /> You can{' '}
             <b>play a card, discard a card,</b> or{' '}
             <b>give a hint to another player.</b>
           </p>
-          <p className="walkthrough-text">
+          <p className="walkthrough-text" style={{ margin: '0' }}>
             Your goal is to <b>stack consecutive cards of the same color</b> -
             like solitare. <br />Let's watch a few turns.
           </p>
@@ -24,8 +35,9 @@ const Walkthrough = props => {
         <div className="walkthrough-div">
           <p className="walkthrough-text">
             Here, Ava doesn't know what's in her hand, so{' '}
-            <b>Ava gives Hal a hint.</b> <br /> To give a hint, she{' '}
-            <b>identifies all cards of a given color or value</b> in Hal's hand.
+            <b>Ava gives Hal a hint.</b> <br /> All hints must take a specific
+            form: Ava must <b>identify all cards of a given color or value</b>{' '}
+            in Hal's hand.
           </p>
           <video
             className="walkthrough-clip"
@@ -50,8 +62,10 @@ const Walkthrough = props => {
         <br />
         <div className="walkthrough-div">
           <p className="walkthrough-text">
-            Hal knows he has a couple of twos, but he doesn't know their color.<br />
-            He plays a red two - but <b>playing a red two is illegal,</b> <br />since{' '}
+            Here, Hal knows he has a couple of twos, but he doesn't know their
+            color.<br />
+            He tries to play a red two - but{' '}
+            <b>playing a red two is illegal,</b> <br />since{' '}
             <b>
               a red one hasn't been played yet.<br />
             </b>The game's <b>fuse</b> decreases by one, and the red two is{' '}
@@ -67,7 +81,8 @@ const Walkthrough = props => {
         <div className="walkthrough-div">
           <p className="walkthrough-text">
             Every time a hint is given, an <b>information token</b> is used.
-            Players can <b>discard to recover information tokens.</b>
+            Players can <b>discard to recover information tokens.</b> Watch the
+            number of tokens available increase from 6 to 7.
           </p>
           <video
             className="walkthrough-clip"
@@ -77,8 +92,9 @@ const Walkthrough = props => {
         </div>
         <div className="walkthrough-div">
           <p className="walkthrough-text" style={{ margin: '0' }}>
-            The game ends when your <b>fuse hits zero.</b> Your score is the
-            number of cards you successfully played.
+            The game ends when your{' '}
+            <b>fuse hits zero or you successfully play five of each color.</b>{' '}
+            Your score is the number of cards successfully played.
           </p>
         </div>
       </div>
