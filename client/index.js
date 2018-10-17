fc23b6d7a4e94113d7f7b8fdce67c4331d43f20f
Andrew Miller <admiller9@gmail.com>
2018-10-17 17:52:59 -0400

    uninstall logrocket

--- client/index.js
+++ client/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogRocket from 'logrocket';
 
 import App from './app';
 import './socket';
-
-LogRocket.init('w23csi/hanabi');
 
 ReactDOM.render(<App />, document.getElementById('app'));
